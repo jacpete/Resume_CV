3962f9d1d4000a4052a4a608285bb8b157a0240d
jacpete <jacobmpeterson12@gmail.com>
2020-03-05 20:46:37 -0600

    add pdf conversion script

---
+++ htmlToPDF.R
@@ -0,0 +1,3 @@
+library(pagedown)
+pagedown::chrome_print("resume.html", output = "resume_Peterson.pdf", format = "pdf")
+pagedown::chrome_print("cv_Peterson.html", output = "cv_Peterson.pdf", format = "pdf")
